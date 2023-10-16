b2831299039a69481a490396a225e88d6da1499d
2406Bilal <bilolismonov6@gmail.com>
2023-10-16 04:50:58 -0700

    addind new files

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <button @click="start"> let's play</button>
+    <Block />
+  </div>
+</template>
+
+<script>
+import Block from "./components/blockcomponents.vue";
+export default {
+  name: "App",
+  components: {
+    Block,
+  },
+  data() {
+    return {
+      isPlaying: false,
+      delay: null,
+    };
+  },
+  methods: {
+    start() {
+      this.delay = 2000 + Math.random() * 5000;
+      this.isPlaying = true;
+      console.log(this.delay);
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #444;
+  margin-top: 60px;
+}
+</style>
